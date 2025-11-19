81cd40cb6bfaae0f07768860e0785f935c07062c
Oleksii Novikov <oleksii@novikraft.com>
2025-11-20 00:30:13 +0200

    feat: creates astro dev container

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const isDev = process.env.NODE_ENV === 'development';
+const isContainer = isDev && process.env.SERVER_HOST === '0.0.0.0';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+      host: process.env.SERVER_HOST || '127.0.0.1',
+      port: Number.parseInt(process.env.SERVER_PORT || '4312', 10),
+  },
+  vite: {
+    server: {
+      watch: isContainer ? { usePolling: true, interval: 300 } : undefined,
+    },
+  },
+});
